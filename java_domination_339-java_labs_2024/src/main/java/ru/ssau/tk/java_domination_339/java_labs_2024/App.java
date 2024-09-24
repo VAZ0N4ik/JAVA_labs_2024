610c01ff2ba76b129cf59489c1f95349e444e9a6
Zaitsev <vazonon@yandex.ru>
2024-09-24 18:27:19 +0400

    create project

---
+++ java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/App.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.java_domination_339.java_labs_2024;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
