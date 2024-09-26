a3f4e4cb00b2d73b799cf5d3470c543abd0d214e
Zaitsev <vazonon@yandex.ru>
2024-09-27 01:10:54 +0400

    Task 1.3 SqrFunction added - Y

---
+++ java_domination_339-java_labs_2024/src/test/java/ru/ssau/tk/java_domination_339/java_labs_2024/functions/SqrFunctionTest.java
@@ -0,0 +1,16 @@
+package ru.ssau.tk.java_domination_339.java_labs_2024.functions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SqrFunctionTest {
+    SqrFunction SF;
+
+    @Test
+    void test(){
+        SF = new SqrFunction();
+        Assertions.assertEquals(10000, SF.apply(100), 0.00000000001);
+        Assertions.assertEquals(0, SF.apply(0), 0.00000000001);
+        Assertions.assertEquals(0.0000000001, SF.apply(0.00001), 0.00000000001);
+    }
+}
