2c37a6fac624686cd7c72f8e0c6346868a71c5d6
Viktor Zaitsev <vazonon@yandex.ru>
2024-10-02 21:56:03 +0400

    ConstantFunctionTest coverage fix

--- java_domination_339-java_labs_2024/src/test/java/ru/ssau/tk/java_domination_339/java_labs_2024/functions/ConstantFunctionTest.java
+++ java_domination_339-java_labs_2024/src/test/java/ru/ssau/tk/java_domination_339/java_labs_2024/functions/ConstantFunctionTest.java
@@ -7,10 +7,24 @@ class ConstantFunctionTest {
     ConstantFunction CF;
 
     @Test
-    void test(){
+    void test1() {
         CF = new ConstantFunction(0.00001);
         Assertions.assertEquals(0.00001, CF.apply(1000), 1e-9);
         Assertions.assertEquals(0.00001, CF.apply(68504.5664654), 1e-9);
         Assertions.assertEquals(0.00001, CF.apply(-0.0001), 1e-9);
+    }
+
+    @Test
+    void test2() {
+        CF = new ConstantFunction(100);
+        Assertions.assertEquals(100, CF.apply(1000), 1e-9);
+        Assertions.assertEquals(100, CF.apply(68504.5664654), 1e-9);
+        Assertions.assertEquals(100, CF.apply(-0.0001), 1e-9);
+    }
+
+    @Test
+    void test3() {
+        CF = new ConstantFunction(1101);
+        Assertions.assertEquals(1101, CF.getConstant(), 1e-9);
     }
 }
