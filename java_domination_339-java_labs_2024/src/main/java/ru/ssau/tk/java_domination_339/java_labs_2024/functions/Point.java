2ff284f593581158824bc588335a6316ec1d885e
Zaitsev <vazonon@yandex.ru>
2024-10-04 20:59:52 +0400

    Task 1.2 Point - Y

---
+++ java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/functions/Point.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.java_domination_339.java_labs_2024.functions;
+
+public class Point {
+    public final double x;
+    public final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
