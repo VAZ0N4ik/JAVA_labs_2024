686c94acc1e455203039eb6c7e7c9fc01e3dda20
Viktor Zaitsev <vazonon@yandex.ru>
2024-10-08 21:41:32 +0400

    Task 3.3.4 TabulatedFunctionFileWriter - Y

--- java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/io/FunctionsIO.java
+++ java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/io/FunctionsIO.java
@@ -1,7 +1,26 @@
 package ru.ssau.tk.java_domination_339.java_labs_2024.io;
 
+import ru.ssau.tk.java_domination_339.java_labs_2024.functions.TabulatedFunction;
+import ru.ssau.tk.java_domination_339.java_labs_2024.functions.Point;
+
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
     }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+        PrintWriter printWriter = new PrintWriter(writer);
+
+        printWriter.println(function.getCount());
+
+        for (Point point : function) {
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+
+        printWriter.flush();
+    }
+
 }
