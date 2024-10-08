dcdfce2d10849cd3d0f6f4242cd7263b5c9e9b4a
Korshikov <korshikovea05@gmail.com>
2024-10-09 01:45:28 +0400

    Task 3.2.7 + 3.2.9  - X

--- java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/functions/factory/TabulatedFunctionFactory.java
+++ java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/functions/factory/TabulatedFunctionFactory.java
@@ -1,8 +1,17 @@
 package ru.ssau.tk.java_domination_339.java_labs_2024.functions.factory;
 
+import ru.ssau.tk.java_domination_339.java_labs_2024.functions.StrictTabulatedFunction;
 import ru.ssau.tk.java_domination_339.java_labs_2024.functions.TabulatedFunction;
+import ru.ssau.tk.java_domination_339.java_labs_2024.functions.UnmodifiableTabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create (double[] xValues, double[] yValues);
 
+    default TabulatedFunction createStrict (double[] xValues, double[] yValues){
+        return new StrictTabulatedFunction(create(xValues,yValues));
+    }
+
+    default TabulatedFunction createStrictUnmodifiable (double[] xValues, double[] yValues){
+        return new StrictTabulatedFunction(new UnmodifiableTabulatedFunction(create(xValues,yValues)));
+    }
 }
