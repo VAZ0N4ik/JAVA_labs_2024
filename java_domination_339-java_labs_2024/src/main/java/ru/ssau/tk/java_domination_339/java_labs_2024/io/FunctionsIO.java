f58c1fcc97aec3963f2058f3f8f6897430a4a31b
Viktor Zaitsev <vazonon@yandex.ru>
2024-10-08 16:35:55 +0400

    Task 3.3.1 FunctionsIO - Y

---
+++ java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/io/FunctionsIO.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.java_domination_339.java_labs_2024.io;
+
+public final class FunctionsIO {
+    private FunctionsIO() {
+        throw new UnsupportedOperationException();
+    }
+}
