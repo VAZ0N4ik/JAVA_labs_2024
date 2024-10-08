eda7c551813e419ea5bcf67c797e7f7a5ebeec88
Viktor Zaitsev <vazonon@yandex.ru>
2024-10-08 10:58:03 +0400

    Task 3.2.4 TabulatedFunctionOperationService update - Y

--- java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/operations/TabulatedFunctionOperationService.java
+++ java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/operations/TabulatedFunctionOperationService.java
@@ -1,9 +1,30 @@
 package ru.ssau.tk.java_domination_339.java_labs_2024.operations;
 
+import ru.ssau.tk.java_domination_339.java_labs_2024.exceptions.InconsistentFunctionsException;
 import ru.ssau.tk.java_domination_339.java_labs_2024.functions.Point;
 import ru.ssau.tk.java_domination_339.java_labs_2024.functions.TabulatedFunction;
+import ru.ssau.tk.java_domination_339.java_labs_2024.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.java_domination_339.java_labs_2024.functions.factory.TabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService {
+    private TabulatedFunctionFactory factory;
+
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    public TabulatedFunctionOperationService() {
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public TabulatedFunctionFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
         Point[] points = new Point[tabulatedFunction.getCount()];
         int i = 0;
@@ -14,6 +35,40 @@ public class TabulatedFunctionOperationService {
         }
 
         return points;
+    }
+
+    private interface BiOperation {
+        double apply(double u, double v);
+    }
+
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation) {
+        if (a.getCount() != b.getCount())
+            throw new InconsistentFunctionsException();
+
+        Point[] pointsA = TabulatedFunctionOperationService.asPoints(a);
+        Point[] pointsB = TabulatedFunctionOperationService.asPoints(b);
+
+        int size = a.getCount();
+        double[] xValues = new double[size];
+        double[] yValues = new double[size];
+
+        for (int i = 0; i < size; ++i) {
+            if (pointsA[i].x != pointsB[i].x)
+                throw new InconsistentFunctionsException();
+
+            xValues[i] = pointsA[i].x;
+            yValues[i] = operation.apply(pointsA[i].y, pointsB[i].y);
+        }
+
+        return factory.create(xValues, yValues);
+    }
+
+    public TabulatedFunction additionOperation(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u + v);
+    }
+
+    public TabulatedFunction substractionOperation(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u - v);
     }
 
 }
