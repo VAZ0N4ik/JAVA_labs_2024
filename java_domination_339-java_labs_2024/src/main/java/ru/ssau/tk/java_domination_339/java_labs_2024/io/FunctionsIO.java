483d2186752a1455b65b8ab2653cab2a4e59d36b
Viktor Zaitsev <vazonon@yandex.ru>
2024-10-09 22:13:15 +0400

    Task 3.3.7 readTabulatedFunction TabulatedFunctionFileReader - Y

--- java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/io/FunctionsIO.java
+++ java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/io/FunctionsIO.java
@@ -2,9 +2,15 @@ package ru.ssau.tk.java_domination_339.java_labs_2024.io;
 
 import ru.ssau.tk.java_domination_339.java_labs_2024.functions.TabulatedFunction;
 import ru.ssau.tk.java_domination_339.java_labs_2024.functions.Point;
+import ru.ssau.tk.java_domination_339.java_labs_2024.functions.factory.TabulatedFunctionFactory;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 public final class FunctionsIO {
     private FunctionsIO() {
@@ -21,6 +27,27 @@ public final class FunctionsIO {
         }
 
         printWriter.flush();
+    }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        int count = Integer.parseInt(reader.readLine());
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+
+        NumberFormat numberFormatter = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+
+        for (int i = 0; i < count; ++i) {
+            String line = reader.readLine();
+            String[] values = line.split(" ");
+            try {
+                xValues[i] = numberFormatter.parse(values[0]).doubleValue();
+                yValues[i] = numberFormatter.parse(values[1]).doubleValue();
+            } catch (ParseException e) {
+                throw new IOException(e);
+            }
+        }
+
+        return factory.create(xValues, yValues);
     }
 
 }
