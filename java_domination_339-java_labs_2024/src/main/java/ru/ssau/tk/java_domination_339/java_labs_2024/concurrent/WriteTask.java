54d4b13df25f93ebc6918a23dd674a30d6cb5cc5
Korshikov <korshikovea05@gmail.com>
2024-10-12 22:33:07 +0400

    4.1.3 execution - X

---
+++ java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/concurrent/WriteTask.java
@@ -0,0 +1,24 @@
+package ru.ssau.tk.java_domination_339.java_labs_2024.concurrent;
+
+import ru.ssau.tk.java_domination_339.java_labs_2024.functions.ConstantFunction;
+import ru.ssau.tk.java_domination_339.java_labs_2024.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.java_domination_339.java_labs_2024.functions.TabulatedFunction;
+
+public class WriteTask implements Runnable{
+    private TabulatedFunction func;
+    private double value;
+    public WriteTask(TabulatedFunction f, double v){
+        func = f;
+        value = v;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < func.getCount(); i++) {
+            func.setY(i,value);
+            System.out.printf("Writing for index %d complete\n",i);
+        }
+    }
+
+
+}
