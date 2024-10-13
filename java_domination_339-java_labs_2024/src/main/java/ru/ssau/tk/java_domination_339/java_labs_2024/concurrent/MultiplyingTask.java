e28a3168b9024043f025d2458279febd1ffb68ad
Viktor Zaitsev <vazonon@yandex.ru>
2024-10-13 21:18:08 +0400

    Task 4.1.6 MultiplyingTask - Y

---
+++ java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/concurrent/MultiplyingTask.java
@@ -0,0 +1,19 @@
+package ru.ssau.tk.java_domination_339.java_labs_2024.concurrent;
+
+import ru.ssau.tk.java_domination_339.java_labs_2024.functions.TabulatedFunction;
+
+public class MultiplyingTask implements Runnable {
+    private final TabulatedFunction function;
+
+    public MultiplyingTask(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    public void run() {
+        for (int i = 0; i < function.getCount(); ++i) {
+            function.setY(i, function.getY(i) * 2);
+        }
+
+        System.out.println("Thread \"" + Thread.currentThread().getName() + "\" finished the task");
+    }
+}
