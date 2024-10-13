4be85153315d461700154f8d6abefd28cf363650
Viktor Zaitsev <vazonon@yandex.ru>
2024-10-13 21:36:40 +0400

    Task 4.1.9 check if thread is finished instead of sleep() - Y

--- java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/concurrent/MultiplyingTask.java
+++ java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/concurrent/MultiplyingTask.java
@@ -4,6 +4,7 @@ import ru.ssau.tk.java_domination_339.java_labs_2024.functions.TabulatedFunction
 
 public class MultiplyingTask implements Runnable {
     private final TabulatedFunction function;
+    private volatile boolean isFinished = false;
 
     public MultiplyingTask(TabulatedFunction function) {
         this.function = function;
@@ -16,6 +17,11 @@ public class MultiplyingTask implements Runnable {
             }
         }
 
+        isFinished = true;
         System.out.println("Thread \"" + Thread.currentThread().getName() + "\" finished the task");
+    }
+
+    public boolean isFinished() {
+        return isFinished;
     }
 }
