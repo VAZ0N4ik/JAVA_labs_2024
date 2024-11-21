2c8089753aa0b62be1569df35de85c8cfdf438d4
Korshikov <korshikovea05@gmail.com>
2024-11-21 17:32:24 +0400

    6.1 entity fix - X

--- java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/entities/builder/MathFunctionEntityBuilder.java
+++ java_domination_339-java_labs_2024/src/main/java/ru/ssau/tk/java_domination_339/java_labs_2024/entities/builder/MathFunctionEntityBuilder.java
@@ -4,17 +4,19 @@ import ru.ssau.tk.java_domination_339.java_labs_2024.dto.MathFunctionDto;
 import ru.ssau.tk.java_domination_339.java_labs_2024.dto.builder.PointDtoBuilder;
 import ru.ssau.tk.java_domination_339.java_labs_2024.entities.MathFunctionEntity;
 
+import java.time.Instant;
+
 public class MathFunctionEntityBuilder {
 
     static public MathFunctionEntity makeMathFunctionEntity(MathFunctionDto dto) {
-        return MathFunctionEntity.builder()
+        return MathFunctionEntity.builder().name(dto.getName())
                 .hash(dto.getHashFunction())
                 .points(dto.getPoints()
                         .stream()
                         .map(PointEntityBuilder::makePointEntity)
                         .toList())
-                .createdAt(dto.getCreatedAt())
-                .updateAt(dto.getUpdateAt())
+                .createdAt(Instant.now())
+                .updateAt(Instant.now())
                 .build();
     }
 }
